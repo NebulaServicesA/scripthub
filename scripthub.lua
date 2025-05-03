4c0e27760862f7681a7a2b70cccba509750fa42b
NebulaServicesA <162523687+NebulaServicesA@users.noreply.github.com>
2025-05-02 19:13:56 -0700

    Create scripthub.lua

---
+++ scripthub.lua
@@ -0,0 +1,13 @@
+local placeId = game.PlaceId
+
+if placeId == 126884695634066 then
+    print("loading grow a garden script")
+loadstring(game:HttpGet("https://pastebin.com/raw/DASdqZA5",true))()
+
+elseif placeId == 2 then
+    print("You are in Game ID 2")
+    -- your code for Game ID 2 here
+
+else
+    print("Unknown game")
+end
