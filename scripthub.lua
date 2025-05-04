accdc0fc650c6f4b91e705797e13c2327a5d03c6
NebulaServicesA <162523687+NebulaServicesA@users.noreply.github.com>
2025-05-04 06:55:39 -0700

    Update scripthub.lua

--- scripthub.lua
+++ scripthub.lua
@@ -1,26 +1,35 @@
 local placeId = game.PlaceId
-local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
-local Notify = AkaliNotif.Notify;
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))()
+local Notify = AkaliNotif.Notify
 
 if placeId == 126884695634066 then
-Notify({
-Description = "Grow A Garden Script Is Being Loaded";
-Title = "Status | Wave 1";
-Duration = 3;
-});
-loadstring(game:HttpGet("https://pastebin.com/raw/DASdqZA5",true))()
+    Notify({
+        Description = "Grow A Garden Game Detected";
+        Title = "Status | Wave 1";
+        Duration = 3;
+    })
+    loadstring(game:HttpGet("https://pastebin.com/raw/DASdqZA5", true))()
 
 elseif placeId == 9865958871 then
     Notify({
-Description = "Big Paintball 2";
-Title = "Status | Wave 1";
-Duration = 3;
-});
-loadstring(game:HttpGet("https://pastebin.com/raw/cg2WmM72",true))()
+        Description = "Big Paintball 2 Game Detected";
+        Title = "Status | Wave 1";
+        Duration = 3;
+    })
+    loadstring(game:HttpGet("https://pastebin.com/raw/cg2WmM72", true))()
+
+elseif placeId == 14259168147 then
+    Notify({
+        Description = "BasketBall Legends Game Detected";
+        Title = "Status | Wave 1";
+        Duration = 3;
+    })
+    loadstring(game:HttpGet("https://pastebin.com/raw/HgtQB7aZ", true))()
+
 else
-        Notify({
-Description = "Unkown Game";
-Title = "Status | Wave 1";
-Duration = 3;
-});
+    Notify({
+        Description = "Unknown Game";
+        Title = "Status | Wave 1";
+        Duration = 3;
+    })
 end
